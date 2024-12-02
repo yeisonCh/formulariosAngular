429966e50906e8a2b99153ca3f7754f0cfbc0282
DESKTOP-QAHOD9B\YEISON CHAPPARO <yeisonchaparro@hotmailc.om>
2024-12-02 11:34:01 -0500

    se agrega validación a los campos email y password, se captura la entrada de teclado del usuario para guardar en localstorage y reutilizar

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import { FormsModule, NgForm, ValueChangeEvent } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,46 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+
+private formulario1 = viewChild.required<NgForm>('formulario');
+private destruirReferencia = inject(DestroyRef);
+
+constructor() {
+  afterNextRender(() => {
+
+    const datoGuardado = window.localStorage.getItem('correo');
+    
+    if(datoGuardado){
+      //JSON.parse obtendremos un objeto de la forma email: value.email
+       const datoGuardadoLocal = JSON.parse(datoGuardado);
+       const  datoEmailGuardado = datoGuardadoLocal.email;
+       //actualizamos el formulario con ese valor optenido con el form que configura angular
+        setTimeout(() => {
+          //en controls hacemos referencia al name de la etiqueta
+        this.formulario1().controls['email'].setValue(datoEmailGuardado);
+        }, 10);
+
+       // para actualizar solo un control 
+       /* si queremos actualizar todos los datos del formulario 
+       this.formulario1().setValue({
+        correo: datoEmailGuardado,
+        contrasena: ''
+       });*/
+
+    }
+
+
+    //usando pipe y la funcion debounceTime, activa la función solo cuando el usuario deja de escribir por 5000 milisegundos 
+   const subscipcion = this.formulario1().valueChanges?.pipe(debounceTime(500)).subscribe({
+      next: (value) => localStorage.setItem('correo', JSON.stringify({email: value.email})),
+        //console.log(value.email),
+    });
+
+    this.destruirReferencia.onDestroy(() => subscipcion?.unsubscribe())
+  })
+}
+
+
   //añadimos el motodo onSubmit que estamos llamando en la plantilla
   onSubmit(datosFormulario: NgForm) {
 
@@ -17,11 +58,22 @@ export class LoginComponent {
       return;
     }
     console.log(datosFormulario);
+    console.log(datosFormulario.touched);
+    console.log('se ha ingresado email '+datosFormulario.controls['email'].touched);
+    console.log('se ha ingresado passsword '+datosFormulario.controls['password'].touched);
+
+
     console.log(datosFormulario.form.value.email)
     console.log(datosFormulario.form.value.password)
+    console.log(datosFormulario.controls['email'].dirty)
 
+
+    //datosFormulario.reset()
+    datosFormulario.form.reset();
+
+}
     
-  }
+ 
 
 
 
